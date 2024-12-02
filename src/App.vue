e7c62826c2ed653968fd91701ef0d6ecc9e0644a
DenisPepper <thech@yandex.ru>
2024-12-02 13:21:36 +0300

    Добавит черновик компонента заметок

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,98 @@
-<template>
-  <div class="counter">
-    <h1 class="counter__header">Counter value is:</h1>
-    <div class="counter__box">
-      <h2 class="counter__value">{{ count }}</h2>
-      <button @click="count -= 1">-</button>
-      <button @click="count += 1">+</button>
-    </div>
-  </div>
-</template>
-
 <script setup>
-import { ref } from 'vue';
-
-const count = ref(0);
+// setup = composition api
 </script>
 
+<template>
+  <section class="notepad">
+    <div class="overlay visually-hidden@">
+      <form class="modal">
+        <textarea name="desc" cols="50" rows="10" placeholder="Введите описание заметки"></textarea>
+        <button type="submit" class="btn-save">save</button>
+      </form>
+    </div>
+    <h2 class="notepad__header">Notes</h2>
+    <button type="button" class="btn-add-new">+</button>
+    <ul class="notepad__list">
+      <li class="note">
+        <span class="note__desc">Описание карточки 1</span>
+        <span class="note__date">01.02.2024</span>
+      </li>
+      <li class="note">
+        <span class="note__desc">Описание карточки 2</span>
+        <span class="note__date">01.02.2024</span>
+      </li>
+      <li class="note">
+        <span class="note__desc">Описание карточки 3</span>
+        <span class="note__date">01.02.2024</span>
+      </li>
+    </ul>
+  </section>
+</template>
+
 <style>
-.counter {
-  max-width: 50vw;
+.notepad {
+  position: relative;
+  max-width: 80vw;
+  min-height: 80vh;
+  margin: 0 auto;
   box-sizing: border-box;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   background-color: rgb(207, 197, 197);
 }
 
-.counter__header {
+.overlay {
+  position: absolute;
+  z-index: 10;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+}
+
+.modal {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+
+.notepad__header {
   text-align: center;
   margin: 0;
   padding: 0.5rem;
 }
 
-.counter__box {
-  display: grid;
-  grid-template-columns: repeat(2, max-content);
+.notepad__list {
+  list-style: none;
+  margin: 1rem;
+  margin-top: 2rem;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  gap: 1rem;
 }
 
-.counter__value {
-  grid-row: 1;
-  grid-column: 1/-1;
-  text-align: center;
+.note {
+  padding: 1rem;
+  min-width: 80%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(2, 1fr);
+  gap: 1rem;
+
+  background-color: goldenrod;
+}
+
+.note__date {
+  font-size: 1rem;
+  font-weight: 700;
 }
 
 button {
-  grid-row: 2;
-
-  width: 10rem;
-  height: 3rem;
+  border: none;
   padding: 0.5rem;
 
   display: flex;
@@ -54,8 +101,33 @@ button {
 
   background-color: black;
   color: goldenrod;
-  border: none;
 
   font-size: 2rem;
+}
+
+.btn-add-new {
+  width: 3rem;
+  height: 3rem;
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+}
+
+.btn-save {
+  color: black;
+  background-color: goldenrod;
+}
+
+.visually-hidden {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  white-space: nowrap;
+  clip-path: inset(100%);
+  clip: rect(0 0 0 0);
+  overflow: hidden;
 }
 </style>
